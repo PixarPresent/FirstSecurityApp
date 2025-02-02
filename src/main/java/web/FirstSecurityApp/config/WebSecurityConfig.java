447d71bd25d2729cfe6808cde4f9ee67e2eb50fc
PixarPresent <pixarpresent@gmail.com>
2025-02-02 22:33:25 +0300

    html pages

--- src/main/java/web/FirstSecurityApp/config/WebSecurityConfig.java
+++ src/main/java/web/FirstSecurityApp/config/WebSecurityConfig.java
@@ -2,7 +2,10 @@ package web.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import web.FirstSecurityApp.security.AuthProviderImpl;
 
@@ -19,7 +22,16 @@ public class WebSecurityConfig {
         this.authProvider = authProvider;
     }
 
+
+    //настройка аутентификации
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login").loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/login?error=true");
+
     }
 }
